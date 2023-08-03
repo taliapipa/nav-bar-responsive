c2bf29b1be9eef90b12d48a6fcb9e1e15bf4e783
taliapipa <125282306+taliapipa@users.noreply.github.com>
2023-08-03 17:47:04 +0200

    structure added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className='navBar'>
+        <h1><AiOutlineMenu/></h1>
+        <h1><AiOutlineClose/></h1>
+        <div className='elementsNav'>
+          <a href=''>Home</a>
+          <a href=''>List</a>
+          <a href=''>About</a>
+        </div>
+      </nav>
     </div>
   );
 }
